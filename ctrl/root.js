712f36cded15e7820fe007f3f6109bb097935023
Nick Thomas <nichoth@gmail.com>
2016-11-11 22:55:34 -0500

    dont re-fetch

--- ctrl/root.js
+++ ctrl/root.js
@@ -4,21 +4,34 @@ var cat = require('pull-cat')
 var flatMerge = require('pull-flat-merge')
 var fnToStream = require('../lib/api-stream')
 
-module.exports = function RootController (api) {
-    return function rootController (params) {
-        var apiMap = {
-            a: 'fetch',
-            b: 'update',
-            c: 'delete'
-        }
+var apiMap = {
+    a: 'fetch',
+    b: 'update',
+    c: 'delete'
+}
 
-        // map view events to api calls
+module.exports = function RootController (api) {
+    var fetched = false
+    var m = many()
+
+    return function rootController (params) {
+
         return S(
+            // map view events to api calls
             S.map(function (ev) {
                 return apiMap[ev]
             }),
+
+            // don't re-fetch from the api if we already have data
+            S.filter(function (ev) {
+                if ((ev === 'fetch') && fetched) return false
+                return true
+            }),
             fnToStream(api),
-            flatMerge()
+            flatMerge(),
+            S.through(function (ev) {
+                fetched = !fetched ? ev.type === 'fetch' : fetched
+            })
         )
     }
 }
