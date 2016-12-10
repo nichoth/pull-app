5f5b6bdcd15033e90df3541255f5492fb6227c09
Nick Thomas <nichoth@gmail.com>
2016-12-10 15:42:37 -0800

    refactor app file

--- example/app.js
+++ example/app.js
@@ -1,62 +1,63 @@
 var React = require('react')
 var reactDom = require('react-dom')
 var S = require('pull-stream')
-var scan = require('pull-scan')
-var router = require('pull-routes')()
+// var scan = require('pull-scan')
+var Router = require('./router')
 
-var wsStream = require('./mock/socket')()
-var api = require('./mock/api')()
-var apiStream = require('./lib/api-stream')(api)
+// var wsStream = require('./mock/socket')()
+// var api = require('./mock/api')()
+// var apiStream = require('./lib/api-stream')(api)
 var rootView = require('./view/root')
-var rootController = require('./ctrl/root')(apiStream)
-var rootStore = require('./store/root')()
+// var rootController = require('./ctrl/root')(apiStream)
+// var rootStore = require('./store/root')()
 
-var wsMap = S.map(function (ev) {
-    return {
-        type: 'ws',
-        data: ev
-    }
-})
-var wss = S(
-    wsStream,
-    wsMap
-)
-rootController.add(wss)
 
-var rStream = router([
+var router = Router([
     ['/', function rootRoute (params) {
-        var strms = [
-            rootView(),
-            rootController(),
-            rootStore()
-        ]
-        return strms
+        return rootView()
+        // var strms = [
+        //     rootView(),
+        //     rootController(),
+        //     rootStore()
+        // ]
+        // return strms
     }],
     ['/foo', function fooRoute (params) {
-        return [
-            require('./view/foo')()
-        ]
+        return require('./view/foo')()
+        // return [
+        //     require('./view/foo')()
+        // ]
     }]
 ])
 
 var el = document.createElement('div')
 document.body.appendChild(el)
+
 S(
-    rStream,
-    scan(function unsubscribe (prev, next) {
-        if (prev) prev[0].sink.abort()
-        return next.fn(next.params)
-    }, null),
-    S.map(function subscribe (ss) {
-        var view = ss[0]
-        var transform = S.apply(null, ss.slice(1, ss.length))
-        if (transform) S( view, transform, view )
-        else S(view, view)
-        return view.view
-    }),
+    router(),
     S.drain(function render (view) {
         reactDom.render(React.createElement(view), el)
     }, function onEnd (err) {
         console.log('error', err)
     })
 )
+
+// S(
+//     rStream,
+//     scan(function unsubscribe (prev, next) {
+//         if (prev) prev[0].sink.abort()
+//         return next.fn(next.params)
+//     }, null),
+//     S.map(function subscribe (ss) {
+//         var view = ss[0]
+//         var transform = S.apply(null, ss.slice(1, ss.length))
+//         if (transform) S( view, transform, view )
+//         else S(view, view)
+//         return view.view
+//     }),
+//     S.drain(function render (view) {
+//         reactDom.render(React.createElement(view), el)
+//     }, function onEnd (err) {
+//         console.log('error', err)
+//     })
+// )
---
+++ example/router.js
@@ -0,0 +1,43 @@
+var S = require('pull-stream')
+var Router = require('pull-router')
+var RootController = require('./ctrl/root')
+var RootStore = require('./store/root')
+var ApiStream = require('./lib/api-stream')
+var Api = require('./mock/api')
+var WsStream = require('./mock/socket')
+
+module.exports = function AppRouter () {
+    var apiStream = ApiStream(Api())
+    var wsStream WsStream()
+    var rootStore = RootStore()
+    var rootController = RootController(apiStream)
+
+    var wsMap = S.map(function (ev) {
+        return {
+            type: 'ws',
+            data: ev
+        }
+    })
+    var wss = S(
+        wsStream,
+        wsMap
+    )
+    rootController.add(wss)
+
+
+    return Router([
+        ['/', function rootRoute (params, rt) {
+            // transform stream
+            var strms = S(
+                rootController(),
+                rootStore()
+            )
+            return strms
+        }],
+        ['/foo', function fooRoute (params) {
+            // do nothing
+            return S.through()
+        }]
+    ])
+}
+
