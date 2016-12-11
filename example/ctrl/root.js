6b5eaf4ac0057ef8d11df6daec2618754a191fd6
Nick Thomas <nichoth@gmail.com>
2016-12-10 18:15:14 -0800

    fix routing partial sink thing

--- example/ctrl/root.js
+++ example/ctrl/root.js
@@ -1,6 +1,11 @@
 var S = require('pull-stream')
+var Abortable = require('pull-abortable')
+var many = require('pull-many')
+// var cat = require('pull-cat')
 var flatMerge = require('pull-flat-merge')
-var Ctrl = require('../../ctrl')
+var onAbort = require('pull-on-abort')
+// var fnToStream = require('../lib/api-stream')
+// var ApiStream = require('../lib/api-stream')
 
 var apiMap = {
     a: 'fetch',
@@ -10,23 +15,42 @@ var apiMap = {
 
 module.exports = function RootController (apiStream) {
     var fetched = false
+    var m = many()
 
-    var transform = S(
-        // map view events to api calls
-        S.map(function (ev) {
-            return apiMap[ev]
-        }),
-        // don't re-fetch from the api if we already have data
-        S.filter(function (ev) {
-            return !(ev === 'fetch' && fetched)
-        }),
-        apiStream(),
-        flatMerge(),
-        S.through(function (ev) {
-            fetched = !fetched ? ev.type === 'fetch' : fetched
-        })
-    )
+    function rootController (params) {
+        var transform = S(
+            // map view events to api calls
+            S.map(function (ev) {
+                return apiMap[ev]
+            }),
 
-    var controller = Ctrl(transform)
-    return controller
+            // don't re-fetch from the api if we already have data
+            S.filter(function (ev) {
+                return !(ev === 'fetch' && fetched)
+            }),
+            apiStream(),
+            // fnToStream(api),
+            flatMerge(),
+            S.through(function (ev) {
+                fetched = !fetched ? ev.type === 'fetch' : fetched
+            })
+        )
+
+        // source here is the events from the view
+        return function (source) {
+            var abortable = Abortable()
+            m.add(S(source, transform, abortable))
+            return S(
+                m,
+                onAbort(function () {
+                    // abort only the view source, not websocket
+                    abortable.abort()
+                    return null
+                })
+            )
+        }
+    }
+
+    rootController.add = m.add.bind(m)
+    return rootController
 }
